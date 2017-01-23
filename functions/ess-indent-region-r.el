5c4a6cacef36ab96fe386bf226136c4bf210832e
Mathieu Basille <basille@ufl.edu>
2017-01-22 21:14:36 -0500

    R indentation (work in progress).

---
+++ functions/ess-indent-region-r.el
@@ -0,0 +1,58 @@
+;; https://www.emacswiki.org/emacs/ESSRParser
+(defun ess-indent-region-as-r (beg end)
+  "Format region of code R using the R parser."
+  (interactive "r")
+  (let ((string (replace-regexp-in-string
+		 "\"" "\\\\\\&"
+		 (replace-regexp-in-string ;; how to avoid this double matching?
+		  "\\\\\"" "\\\\\\&" (buffer-substring-no-properties beg end))))
+	(buf (get-buffer-create "*ess-command-output*")))
+    (ess-force-buffer-current "Process to load into:")
+    (ess-command
+     (format
+      "local({oo <- options(keep.source = FALSE);
+cat('\n', paste(deparse(parse(text = \"%s\")[[1L]]), collapse = '\n'), '\n', sep = '')
+options(oo)})\n"
+      string) buf)
+    (with-current-buffer buf
+      (goto-char (point-max))
+      ;; (skip-chars-backward "\n")
+      (let ((end (point)))
+	(goto-char (point-min))
+	(goto-char (1+ (point-at-eol)))
+	(setq string (buffer-substring-no-properties (point) end))
+	))
+    (delete-region beg end)
+    (insert string)
+    ))
+
+;; From Walmes Zeviani
+;; https://github.com/basille/.emacs.d/issues/1
+
+(defun ess-indent-region-with-formatr (beg end)
+  "Format region of code R using formatR::tidy_source()."
+  (interactive "r")
+  (let ((string
+         (replace-regexp-in-string
+          "\"" "\\\\\\&"
+          (replace-regexp-in-string ;; how to avoid this double matching?
+           "\\\\\"" "\\\\\\&"
+           (buffer-substring-no-properties beg end))))
+	(buf (get-buffer-create "*ess-command-output*")))
+    (ess-force-buffer-current "Process to load into:")
+    (ess-command
+     (format                            ; R parser use 'width.cutoff = 60L'
+      "local({formatR::tidy_source(text = \"\n%s\", arrow = TRUE, width.cutoff = 60L) })\n"
+      string) buf)
+    (with-current-buffer buf
+      (goto-char (point-max))
+      ;; (skip-chars-backward "\n")
+      (let ((end (point)))
+	(goto-char (point-min))
+	(goto-char (1+ (point-at-eol)))
+	(setq string (buffer-substring-no-properties (point) end))
+	))
+    (delete-region beg end)
+    (insert string)
+    (delete-char -1)
+    ))
