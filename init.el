65d918d20d42e619ac3248b83d1cdc469f52b7c2
Mathieu Basille <basille@ufl.edu>
2017-06-19 23:43:33 -0400

    Client update.

--- init.el
+++ init.el
@@ -42,7 +42,7 @@ WantedBy=default.target
 ;; Create a Desktop entry for Emacs client
 (write-region "[Desktop Entry]
 Version=1.0
-Name=GNU Emacs 25 (client)
+Name=GNU Emacs 25 (Client)
 GenericName=Text Editor
 Comment=GNU Emacs using the daemon
 MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
