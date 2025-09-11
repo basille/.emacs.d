c2ce5a7455a08df05b0e705bfe594560c93aa62f
Mathieu Basille <mathieu@basille.org>
2025-09-11 13:53:23 +0200

    start-here function & md-todo-library major update.

--- functions/md-todo-library.el
+++ functions/md-todo-library.el
@@ -1,50 +1,47 @@
 ;; To-do library for Markdown
-;; Author: Mathieu Basille
+;; Author: Mathieu Basille & ChatGPT
 
-;; Function md-todo to insert **TODO** at beginning of line.
-(defun md-todo ()
-  "Insert **TODO** at beginning of line in Markdown."
+(require 'cl-lib)
+
+(defcustom md-todo-states '("**TODO**" "**DONE**" "**CANCEL**" "")
+  "List of states cycled by `md-cycle-todo'."
+  :type '(repeat string)
+  :group 'markdown)
+
+(defun md-todo-cycle ()
+  "Cycle TODO states at the start of a Markdown list line.
+Cursor follows the text if a keyword is added or removed."
   (interactive)
-  (save-excursion
-    (beginning-of-line)
-    (forward-word 1)
-    (backward-word 1)
-    (insert "**TODO** ")))
+  (let* ((pos (point))
+         (line-beg (line-beginning-position)))
+    (save-excursion
+      (goto-char line-beg)
+      (skip-chars-forward " \t")
+      ;; Skip list marker if present
+      (when (looking-at "\\([*+-]\\|[0-9]+[.)]\\)\\s-+")
+        (goto-char (match-end 0)))
+      ;; Compute cursor offset from current point
+      (let ((offset (- pos (point))))
+        ;; Check for existing keyword
+        (let ((match (cl-find-if (lambda (s) (looking-at (regexp-quote s)))
+                                 md-todo-states)))
+          (let ((next (if match
+                          (nth (mod (1+ (cl-position match md-todo-states :test #'string=))
+                                     (length md-todo-states))
+                               md-todo-states)
+                        (car md-todo-states))))
+            ;; Remove existing keyword + following space
+            (when match
+              (delete-region (match-beginning 0) (match-end 0))
+              (when (looking-at " ") (delete-char 1)))
+            ;; Insert next state if non-empty
+            (unless (string= next "")
+              (insert next)
+              (unless (or (eolp) (looking-at " "))
+                (insert " "))))
+          ;; Restore cursor relative to text
+          (goto-char (+ (point) offset)))))))
 
-;; Function md-todo-done to replace **TODO** in current line by **DONE**.
-(defun md-todo-done ()
-  "Switch from TODO to DONE."
-  (interactive)
-  (save-excursion
-    (beginning-of-line)
-    (while (search-forward "**TODO**" (line-end-position) 0)
-        (replace-match "**DONE**" 1))))
-
-;; Function md-todo-cancel to replace **TODO** in current line by **CANCEL**.
-(defun md-todo-cancel ()
-  "Switch from TODO to CANCEL."
-  (interactive)
-  (save-excursion
-    (beginning-of-line)
-    (while (search-forward "**TODO**" (line-end-position) 0)
-        (replace-match "**CANCEL**" 1))))
-
-;; ;; Function md-todo-cycle to cycle between **TODO**, **DONE**, **CANCEL** and nothing in current line.
-;; (defun md-todo-cycle ()
-;;   "Cycle between TODO, DONE, CANCEL and nothing."
-;;   (interactive)
-;;   (save-excursion
-;;     (beginning-of-line)
-;;     (while (search-forward "**CANCEL**" (line-end-position) 0)
-;;         (replace-match "" 1))))
-;;     (while (search-forward "**DONE**" (line-end-position) 0)
-;;       (replace-match "**CANCEL**" 1))
-;;     (while (search-forward "**TODO**" (line-end-position) 0)
-;;         (replace-match "**DONE**" 1))))
-;;
-;;     ))
-
-;; Function md-todo-list to display a TODO list with 'rgrep'
 (defun md-todo-list ()
   "Opens a top window with a list of all TODO items in the Notes folder."
   (interactive)
